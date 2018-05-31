7f58f6237bf39128de84b8bad2824a982a720d69
nebulabox <nebulabox@gmail.com>
2018-05-31 20:16:26 +0800

    nomsg

--- update.rebase.sh
+++ update.rebase.sh
@@ -2,7 +2,7 @@
 set -x # echo on
 set -e # exit on error
 git rebase upstream/master
-git commit -m "merge rebase from upstream/master"
+#git commit -m "merge rebase from upstream/master"
 git pull
 git push
 
